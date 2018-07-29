73cd830ec308691b1193b9941d0982ce401e042f
Hua David <darklit@Huade-MacBook-Pro.local>
2018-07-29 23:48:30 +0800

    table view 建立時間section

--- AddcnBadminton/Fight/FightViewController.swift
+++ AddcnBadminton/Fight/FightViewController.swift
@@ -10,29 +10,68 @@ import UIKit
 
 class FightViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
-    let dateFormatterPrinter = DateFormatter()
-    
     struct dataStruct {
         var teamOneName:String
         var teamTwoName:String
-        var createdTime:Date
+        var createdTime:String
     }
     
     var data:[dataStruct] = [
-        dataStruct(teamOneName: "王大明&李小美", teamTwoName: "艾昇華&阿土伯", createdTime: Date()),
-        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: Date())
+        dataStruct(teamOneName: "王大明&李小美", teamTwoName: "艾昇華&阿土伯", createdTime: "2017-06-01"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2017-06-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2017-07-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2017-08-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2017-09-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2017-09-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2017-09-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2018-06-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2017-09-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2017-09-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2018-06-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2018-06-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2018-06-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2018-06-02"),
+        dataStruct(teamOneName: "阿土伯&李小美", teamTwoName: "王大明&艾昇華", createdTime: "2018-06-02")
     ]
     
+    var sectionsInTable:[String] = []
+    
+    func getSectionItems(section: Int) -> [dataStruct] {
+        var sectionItems = [dataStruct]()
+        
+        for item in data {
+            if item.createdTime == sectionsInTable[section] {
+                sectionItems.append(item)
+            }
+        }
+        
+        return sectionItems
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        sectionsInTable = Array(Set(data.map { $0.createdTime }))
+        return sectionsInTable[section]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        sectionsInTable = Array(Set(data.map { $0.createdTime }))
+        return sectionsInTable.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return self.getSectionItems(section: section).count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FightTableViewCell
-        dateFormatterPrinter.dateFormat = "yyyy-MM-dd"
-        cell.teamOneLabel.text = data[indexPath.row].teamOneName
-        cell.teamTwoLabel.text = data[indexPath.row].teamTwoName
-        cell.fightTimeLabel.text = dateFormatterPrinter.string(from: data[indexPath.row].createdTime)
+        // get the items in this section
+        let sectionItems = self.getSectionItems(section: indexPath.section)
+        // get the item for the row in this section
+        let dateTextItem = sectionItems[indexPath.row]
+
+        cell.teamOneLabel.text = dateTextItem.teamOneName
+        cell.teamTwoLabel.text = dateTextItem.teamTwoName
+        cell.fightTimeLabel.text = dateTextItem.createdTime
         
         return cell
     }
